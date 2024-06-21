5ae01367a2d45da0ca5297dde5ca192f4f025970
Syed Misbah Ur Rehman <syedmisbah588@gmail.com>
2024-06-21 19:53:09 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-amber-600">Hello, TCN!</h1>
+    </div>
+  );
+}
+
+export default App;
